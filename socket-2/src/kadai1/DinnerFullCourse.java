2e65d2597c12b87362c391dcd3c62b815980f609
Daikan2525 <22fi016.dendai@gmail.com>
2024-05-23 10:25:29 +0900

    add

--- socket-2/src/kadai1/DinnerFullCourse.java
+++ socket-2/src/kadai1/DinnerFullCourse.java
@@ -6,15 +6,27 @@ public class DinnerFullCourse {
     DinnerFullCourse(){
         for(int i = 0;i < list.length;i++){
             list[i] = new Dish();
-            list[i].setName("前菜");
-            list[i].setValune(10);
         }
+        list[0].setName("前菜盛り合わせ");
+        list[0].setValune(1000);
+
+        list[1].setName("魚料理");
+        list[1].setValune(1200);
+
+        list[2].setName("肉料理");
+        list[2].setValune(1500);
+
+        list[3].setName("ご飯もの");
+        list[3].setValune(700);
+
+        list[4].setName("デザート");
+        list[4].setValune(600);
     }
 
     void eatAll(){
         String str = "";
         for(Dish dish : list){
-            str += dish.getName() + "," + dish.getValune() + "/" ;
+            str += dish.getName() + "," + dish.getValune() + "円/" ;
         }
         System.out.println("今日のフルコースの内容は" + str + "です。");
     }
